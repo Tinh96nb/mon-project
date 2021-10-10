4e6dc97c2c0eed1b36ccd7506714ce75c1b9048c
PhamTinh <phamductinhkma@gmail.com>
2021-10-10 20:52:52 +0700

    remove address admin

--- server-api/src/helper/web3.js
+++ server-api/src/helper/web3.js
@@ -3,11 +3,11 @@ const TOKENABI = require('../abi/TOKEN_ABI.json')
 const NFTABI = require('../abi/NFT_ABI.json')
 const MARKETABI = require('../abi/MARKET_ABI.json')
 
+console.log(process.env.NETWORK_CONTRACT);
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.NETWORK_CONTRACT))
 
 function contract () {
   if (!web3) return null;
-  web3.eth.accounts.wallet.add(process.env.PRIVATE)
   return {
     token: new web3.eth.Contract(TOKENABI, process.env.CONTRACT_TOKEN),
     nft: new web3.eth.Contract(NFTABI, process.env.CONTRACT_NFT),
