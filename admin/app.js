0545f3c7788064fc5a27840c175c1c593e12a5ee
PhamTinh <phamductinhkma@gmail.com>
2021-10-10 21:02:38 +0700

    remove cookie

--- admin/app.js
+++ admin/app.js
@@ -2,7 +2,6 @@ require('dotenv').config({path: './env-files/production.env'});
 const express = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
-const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
 const initAuthMiddleware = require('./features/login/init-auth-middleware');
@@ -17,21 +16,15 @@ app.use(expressLayouts);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-const { COOKIE_EXPIRATION_MS, PORT } = process.env;
+const { PORT } = process.env;
 app.use(
   session({
     secret: 'keyboard cat',
-    name: process.env.SESSION_COOKIE_NAME,
+    name: "admin mon",
     resave: false,
     saveUninitialized: true,
-    cookie: {
-      secure: process.env.NODE_ENV === 'production',
-      expires: Date.now() + parseInt(COOKIE_EXPIRATION_MS, 10),
-      maxAge: parseInt(COOKIE_EXPIRATION_MS, 10),
-    },
   })
 );
 
