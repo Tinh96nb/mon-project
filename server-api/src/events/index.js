3138bf30f9b706df237e60ac7d729f227e6b5611
PhamTinh.IT <phamductinhkma@gmail.com>
2021-12-23 14:17:42 +0700

    edit event

--- server-api/src/events/index.js
+++ server-api/src/events/index.js
@@ -1,130 +1,95 @@
 require('dotenv').config()
-const fs = require('fs').promises;
-const path = require('path');
+const knex = require('../models/connect');
 const nftModel = require("../models/nft");
 const historyModel = require("../models/nft_history");
-
-const EventEmitter = require("events");
-const event = new EventEmitter();
 const { contract, web3 } = require("../helper/web3");
 
-const EVENT = {
-  minNFT: "MIN_NFT",
-  buy: "BUY",
-  createSell: "CREATE_SELL",
-  cancelOrder: "CANCEL_SELL",
-  setRoy: "SET_ROY",
+const stepSize = 100;
+const stepTimeMs = 2000;
+
+async function findOrCreateLatestBlockCursor(blockHeight, eventName) {
+  const blockCursor = await knex('block_cursor').where({ event_name: eventName }).first();
+  if (blockCursor) return blockCursor;
+  await knex('block_cursor').insert({ event_name: eventName, current_block: blockHeight });
+  const created = await knex('block_cursor').where({ event_name: eventName }).first();
+  return created;
 }
 
-const fileName = path.resolve(__dirname, 'data.txt');
+async function updateBlockCursor(eventName, blockHeight) {
+  return knex('block_cursor').where({ event_name: eventName }).update({ current_block: blockHeight });
+}
 
-async function init() {
-  const data = await fs.readFile(fileName, 'utf8');
-  const objBlock = JSON.parse(data);
+async function getBlockRange(fromBlock) {
+  let toBlock = await web3.eth.getBlockNumber();
+  if (fromBlock + stepSize < toBlock) {
+    toBlock = fromBlock + stepSize;
+  }
+  fromBlock++;
+  return Object.assign({}, { fromBlock, toBlock });
+}
+
+const events = [
+  {
+    name: "Transfer",
+    contractCall: contract().nft,
+    excute: mintNfts
+  },
+  {
+    name: "Purchased",
+    contractCall: contract().market,
+    excute: buy
+  },
+  {
+    name: "NewSellOrderCreated",
+    contractCall: contract().market,
+    excute: createSell
+  },
+  {
+    name: "CancelOrder",
+    contractCall: contract().market,
+    excute: cancelSell
+  },
+  {
+    name: "SetFeeCoppyRight",
+    contractCall: contract().nft,
+    excute: setRoy
+  },
+];
+
+async function main() {
   const latest = await web3.eth.getBlockNumber();
-  const stepBlock = 20;
-  const {
-    [EVENT.minNFT]: blockMint,
-    [EVENT.buy]: blockBuy,
-    [EVENT.createSell]: blockSell,
-    [EVENT.cancelOrder]: blockCancel,
-    [EVENT.setRoy]: blockRoy,
-  } = objBlock
+  console.log('latestBlockHeight =', latest);
+  events.forEach(async ({name: eventName, contractCall, excute}) => {
+    const poll = async () => {
+      const currentblockEvent = await findOrCreateLatestBlockCursor(latest, eventName);
+      const { fromBlock, toBlock } = await getBlockRange(+currentblockEvent.current_block);
+      if (fromBlock <= toBlock) {
+        try {
+          const events = await contractCall.getPastEvents(eventName, { fromBlock, toBlock });
+          if (events.length) {
+            await excute(events);
+          }
+        } catch (e) {
+          console.log(`Error retrieving events for block range fromBlock=${fromBlock} toBlock=${toBlock}`, e);
+          const timer = setTimeout(() => {
+            clearTimeout(timer);
+            poll()
+          }, stepTimeMs);
+          return;
+        }
+        await updateBlockCursor(eventName, toBlock);
+      }
+      const timer = setTimeout(() => {
+        clearTimeout(timer);
+        poll()
+      }, stepTimeMs);
+    };
+    poll();
+  });
+}
+main();
 
-  // event trasfer
-  if (+blockMint === 0) {
-    objBlock[EVENT.minNFT] = latest;
-    await fs.writeFile(fileName, JSON.stringify(objBlock));
-  } else if (+blockMint < +latest) {
-    const to = +blockMint + stepBlock > latest ? latest : +blockMint + stepBlock;
-    contract().nft
-    .getPastEvents("Transfer", {
-      fromBlock: +blockMint,
-      toBlock: to
-    })
-    .then(async (events) => {
-        if (events.length) event.emit(EVENT.minNFT, events)
-        objBlock[EVENT.minNFT] = to+1;
-        await fs.writeFile(fileName, JSON.stringify(objBlock));
-      })
-  }
-
-  // event buy
-  if (+blockBuy === 0) {
-    objBlock[EVENT.buy] = latest;
-    await fs.writeFile(fileName, JSON.stringify(objBlock));
-  } else if (+blockBuy < +latest) {
-    const to = +blockBuy + stepBlock > latest ? latest : +blockBuy + stepBlock;
-    contract().market 
-      .getPastEvents("Purchased", {
-        fromBlock: +blockBuy,
-        toBlock: to
-      })
-      .then(async (events) => {
-        if (events.length) event.emit(EVENT.buy, events)
-        objBlock[EVENT.buy] = to+1;
-        await fs.writeFile(fileName, JSON.stringify(objBlock));
-      })
-  }
-
-  // event create sell
-  if (+blockSell === 0) {
-    objBlock[EVENT.createSell] = latest;
-    await fs.writeFile(fileName, JSON.stringify(objBlock));
-  } else if (+blockSell < +latest) {
-    const to = +blockSell + stepBlock > latest ? latest : +blockSell + stepBlock;
-    contract().market 
-      .getPastEvents("NewSellOrderCreated", {
-        fromBlock: +blockSell,
-        toBlock: to
-      })
-      .then(async (events) => {
-        if (events.length) event.emit(EVENT.createSell, events)
-        objBlock[EVENT.createSell] = to+1;
-        await fs.writeFile(fileName, JSON.stringify(objBlock));
-      })
-  }
-
-  // event cancel sell
-  if (+blockCancel === 0) {
-    objBlock[EVENT.cancelOrder] = latest;
-    await fs.writeFile(fileName, JSON.stringify(objBlock));
-  } else if (+blockCancel < +latest) {
-    const to = +blockSell + stepBlock > latest ? latest : +blockSell + stepBlock;
-    contract().market 
-      .getPastEvents("CancelOrder", {
-        fromBlock: +blockCancel,
-        toBlock: to
-      })
-      .then(async (events) => {
-        if (events.length) event.emit(EVENT.cancelOrder, events)
-        objBlock[EVENT.cancelOrder] = to+1;
-        await fs.writeFile(fileName, JSON.stringify(objBlock));
-      })
-  }
-
-  // event cancel sell
-  if (+blockRoy === 0) {
-    objBlock[EVENT.setRoy] = latest;
-    await fs.writeFile(fileName, JSON.stringify(objBlock));
-  } else if (+blockRoy < +latest) {
-    const to = +blockRoy + stepBlock > latest ? latest : +blockRoy + stepBlock;
-    contract().nft 
-      .getPastEvents("SetFeeCoppyRight", {
-        fromBlock: +blockRoy,
-        toBlock: to
-      })
-      .then(async (events) => {
-        if (events.length) event.emit(EVENT.setRoy, events)
-        objBlock[EVENT.setRoy] = to+1;
-        await fs.writeFile(fileName, JSON.stringify(objBlock));
-      })
-  }
-  setTimeout(() => init(), 2500);
-};
-init();
-
-event.on(EVENT.minNFT, async (events) => {
+async function mintNfts (events) {
   for (let index = 0; index < events.length; index++) {
     const { returnValues, transactionHash } = events[index];
     const { from, to, tokenId } = returnValues
@@ -133,56 +98,40 @@ event.on(EVENT.minNFT, async (events) => {
       console.log(res);
     }
   }
-});
+};
 
-event.on(EVENT.buy, async (events) => {
+async function buy (events) {
   for (let index = 0; index < events.length; index++) {
     const { returnValues, transactionHash } = events[index];
     const { buyer, tokenId, price } = returnValues;
     const res = await historyModel.createHistory(buyer, tokenId, price, transactionHash)
     console.log(res);
   }
-});
+};
 
-event.on(EVENT.createSell, async (events) => {
+async function createSell (events) {
   for (let index = 0; index < events.length; index++) {
     const { returnValues } = events[index];
     const { tokenId, seller, price } = returnValues;
     const res = await nftModel.confirmPriceSell(tokenId, seller, price)
     console.log(res);
   }
-});
+};
 
-event.on(EVENT.cancelOrder, async (events) => {
+async function cancelSell (events) {
   for (let index = 0; index < events.length; index++) {
     const { returnValues } = events[index];
     const { tokenId } = returnValues;
     const res = await nftModel.cancelOrder(tokenId)
     console.log(res);
   }
-});
+}
 
-event.on(EVENT.cancelOrder, async (events) => {
-  for (let index = 0; index < events.length; index++) {
-    const { returnValues } = events[index];
-    const { tokenId } = returnValues;
-    const res = await nftModel.cancelOrder(tokenId)
-    console.log(res);
-  }
-});
-
-event.on(EVENT.setRoy, async (events) => {
+async function setRoy (events) {
   for (let index = 0; index < events.length; index++) {
     const { returnValues } = events[index];
     const { tokenId, feePercent } = returnValues;
     const res = await nftModel.setFeeCopyright(tokenId, feePercent)
     console.log(res);
   }
-});
-
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
 }
